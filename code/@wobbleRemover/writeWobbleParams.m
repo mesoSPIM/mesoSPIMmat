dc2d668a14f7246e4d36ada8959576360f059c0a
Rob Campbell <git@raacampbell.com>
2019-09-16 16:04:33 +0100

    Minor bug fix.

--- code/@wobbleRemover/writeWobbleParams.m
+++ code/@wobbleRemover/writeWobbleParams.m
@@ -8,7 +8,12 @@ function writeWobbleParams(obj)
     lastWobble.wavelength = obj.wavelength;
     lastWobble.offset = min(obj.wobbleModel.zVals);
 
-    SETTINGS_DIR = strrep(which('wobbleRemover'), '@wobbleRemover/wobbleRemover.m','settingsWobbleRemover');
+    SETTINGS_DIR = regexprep(which('wobbleRemover'), '@wobble.*$','settingsWobbleRemover');
+    if ~exist(SETTINGS_DIR,'dir')
+        fprintf('No directory %s for saving last used parameters.\n', SETTINGS_DIR)
+        return
+    end
     fname = fullfile(SETTINGS_DIR,obj.paramsFname);
     save(fname,'lastWobble')
 end %writeWobbleParams
+
